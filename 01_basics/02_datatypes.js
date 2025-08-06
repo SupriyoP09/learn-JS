497bc1a521c222550c8fda635f905aaf4cfc0de6
Supriyo Pramanik <supriyopramanik.2006@gmail.com>
2025-08-06 17:10:08 +0530

    datatypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3 ) // we are using nodejs not a browser
+
+console.log(3+3); // now u will the ans 6
+
+
+let name = "ram"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolen => true/false
+// null => standlone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof"ram"); // string
+console.log(typeof age); // number
+console.log(typeof undefined); // undefined
+
+// now the interesting thing is that what type is null
+
+console.log(typeof null); // object
