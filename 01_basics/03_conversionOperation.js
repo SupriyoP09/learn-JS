1657506741c67109591918bb838abcbb5b9257d7
Supriyo Pramanik <supriyopramanik.2006@gmail.com>
2025-08-06 18:26:59 +0530

    operations and increment done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,100 @@
+let score = false
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Ram"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Ram" => true
+
+let someNumber = 45
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ************************* Operations *************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = "Ram"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 2 + "2");
+// console.log("1" + 2 + 2);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+// console.log(gameCounter);
+
+// ******increment(++)******
+
+// ****Postfix increment****
+
+// let x = 18;
+// const y = x++;
+
+// console.log("x is", x);
+// console.log("y is", y);
+
+// x is 19; y is 18
+
+// let x2 = 18n;
+// const y2 = x2++;
+
+// console.log("x2 is", x2);
+// console.log("y2 is", y2); 
+
+// x2 is 19n; y2 is 18n
+
+// *****Prefix increment*****
+
+let x = 3;
+const y = ++x;
+
+console.log("x is", x);
+console.log("y is", y);
+
+// x is 4; y is 4
+
+let x2 = 3n;
+const y2 = ++x2;
+
+console.log("x2 is", x2);
+console.log("y2 is", y2);
+
+// x2 is 4n; y2 is 4n
