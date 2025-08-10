d55de28d5c2d9f50fe6f6153fc568c86740c4ff3
Supriyo Pramanik <supriyopramanik.2006@gmail.com>
2025-08-10 22:41:17 +0530

    objects part 1 done

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -2,17 +2,39 @@
 
 // object literals
 // Object.create
+
+const mySym = Symbol("key1")
+
 const JsUser = {
     name: "Supriyo",
     "full name": "Supriyo Pramanik",
+    [mySym]: "mykey1",
     age: 18,
     location: "india",
     email: "abc@gmail.com",
+    isLoggedIn: false,
     lastLoginDays: ["Monday", 'Saturday']
 }
 
-console.log(JsUser.email);
-console.log(JsUser["email"]);
-console.log(JsUser"full name");
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
 
+JsUser.email = "abc@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "abc@apple.com"
+// console.log(JsUser);
 
+JsUser.greeting = function(){
+    console.log("hello Js user");
+    
+}
+
+JsUser.greetingTwo = function(){
+    console.log(`hello Js user, ${this.name}`);
+    
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
