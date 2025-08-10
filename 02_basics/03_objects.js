7e66d5072a67bdb5eb41d3ecd65f8e19c1132fcb
Supriyo Pramanik <supriyopramanik.2006@gmail.com>
2025-08-10 21:54:02 +0530

    save

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,18 @@
+// singleton
+
+// object literals
+// Object.create
+const JsUser = {
+    name: "Supriyo",
+    "full name": "Supriyo Pramanik",
+    age: 18,
+    location: "india",
+    email: "abc@gmail.com",
+    lastLoginDays: ["Monday", 'Saturday']
+}
+
+console.log(JsUser.email);
+console.log(JsUser["email"]);
+console.log(JsUser"full name");
+
+
