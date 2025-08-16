95f1da0a36c7eb022b4c5de153d8a1829ac40238
Supriyo Pramanik <supriyopramanik.2006@gmail.com>
2025-08-16 19:05:41 +0530

    functions done

--- 02_basics/05_functions.js
+++ 02_basics/05_functions.js
@@ -19,6 +19,7 @@ function addTwoNumber(number1, number2){
     // return result
     return number1 + number2
 }
+
 addTwoNumber(10, 8)
 addTwoNumber(10, "8")
 addTwoNumber(10, "a")
@@ -28,13 +29,39 @@ const result = addTwoNumber(10, 8)
 
 // console.log("Result:", result);
 
-function loginUserMessage(username){
-    if(username === undefined){
+function loginUserMessage(username = "ram"){
+    if(!username){
         console.log("please enter a username");
+        return
         
     }
     return `${username} just logged in`
 }
 // console.log(loginUserMessage("Supriyo"));
-console.log(loginUserMessage()); // undefined just logged in
+// console.log(loginUserMessage("supriyo"));
 
+function calculateCartPrice(...num1){
+    return num1
+}
+// console.log(calculateCartPrice(18, 45, 7, 17));
+
+const user = {
+    username: 'Supriyo',
+    price: 1800
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+handleObject({
+    username: "barbarian",
+    price: 1200
+})
+
+const myNewArray = [18, 7, 45, 17]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([45, 18, 17, 7]));
