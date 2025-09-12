50e45af110dda67525672151a7271e33d14c6b8e
Supriyo Pramanik <supriyopramanik.2006@gmail.com>
2025-09-12 19:04:20 +0530

    for loops

--- iterations/one.js
+++ iterations/one.js
@@ -12,11 +12,42 @@ for (let i = 0; i <=10; i++) {
 }
 
 for (let i = 0; i <= 5; i++) {
-    console.log(`outer loop value: ${i}`);
+    // console.log(`outer loop value: ${i}`);
     
     for (let j = 0; j <= 5; j++) {
             // console.log(`inner loop value: ${j} and inner loop ${i}`);
-               console.log(i + '*' + j + ' = ' + i*j);
+            //    console.log(i + '*' + j + ' = ' + i*j);
                         
     }    
+}
+let myArray = ["batman", "superman"]
+// console.log(myArray.length);
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+    
+}
+
+// break and continue
+
+// for (let i = 1; i <= 18; i++) {
+//     if (i == 5) {
+//         console.log(`Detected 5`);
+//         break
+        
+//     }
+//     console.log(`Value of i is ${i}`);
+    
+// }
+
+
+for (let i = 1; i <= 18; i++) {
+    if (i == 5) {
+        console.log(`Detected 5`);
+        continue
+        
+    }
+    console.log(`Value of i is ${i}`);
+    
 }
