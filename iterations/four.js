29f0c512b53dfa0c4b3f61e39128afd6337bf681
Supriyo Pramanik <supriyopramanik.2006@gmail.com>
2025-09-14 18:15:02 +0530

    for in loop

---
+++ iterations/four.js
@@ -0,0 +1,17 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    ts: 'typescript'
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+    
+}
+
+const programming = ["js", "ts", "cpp"]
+for (const key in programming) {
+    // console.log(key);
+    // console.log(programming[key]);
+    
+}
